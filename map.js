1c4957971b59b23110a1064869ba8ca88a5ff195
ImpulseLondon <167471115+ImpulseLondon@users.noreply.github.com>
2024-04-18 21:26:00 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
 function setupMap() {
-    var map = L.map('map').setView([51.5136, -0.1310], 15); // Center the map around 56 Dean Street
+    var map = L.map('map').setView([51.5136, -0.1310], 15); // Center on 56 Dean Street
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Adding a marker for 56 Dean Street, a sexual health clinic
+    // Adding a marker for 56 Dean Street
     var marker = L.marker([51.5136, -0.1310]).addTo(map);
     marker.bindPopup('56 Dean Street, London - Sexual Health Clinic');
 }
